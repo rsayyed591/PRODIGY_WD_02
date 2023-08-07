68154fb75f8c273c22168e2e628ce8f18fb615d0
rsayyed591 <125148030+rsayyed591@users.noreply.github.com>
2023-08-07 15:26:32 +0530

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,42 @@
+# Stopwatch App
+
+The Stopwatch App is a simple web application that allows users to measure time intervals accurately and record lap times. It provides basic stopwatch functionalities such as start, pause, reset, and record lap times.
+
+## How to Use
+
+1. Clone or download the repository to your local machine.
+2. Open the `index.html` file in your web browser.
+3. The Stopwatch App interface will be displayed, showing the title "Stopwatch App," a timer display, and control buttons.
+4. To start the stopwatch, click the "Start" button.
+5. To pause the stopwatch, click the "Pause" button.
+6. To reset the stopwatch, click the "Reset" button.
+7. While the stopwatch is running, you can click the "Lap" button to record lap times.
+8. The recorded lap times will be displayed below the stopwatch in a list format.
+
+## Features
+
+- Start: Begins the stopwatch and starts counting time from zero.
+- Pause: Pauses the stopwatch at the current elapsed time.
+- Reset: Stops the stopwatch and resets the timer display to "00:00:00."
+- Lap: Records lap times while the stopwatch is running and displays them in a list below the timer.
+
+## Technologies Used
+
+- HTML: Provides the structure and elements of the web application.
+- CSS: Styles the visual elements and layout of the application.
+- JavaScript: Implements the stopwatch functionalities and timing logic.
+
+## Preview
+
+![Stopwatch App Preview](https://ibb.co/7VZpTwx, https://ibb.co/yBBMLRp)
+
+## Contributions
+
+This Stopwatch App is a basic implementation and is open for contributions and improvements. If you find any issues or have suggestions for enhancements, feel free to create a pull request or open an issue.
+
+---
+
+Developed by Rehan Sayyed
+LinkedIn: https://www.linkedin.com/in/rehan42/
+GitHub: https://github.com/rsayyed591
+HackerRank: https://www.hackerrank.com/rehansayyed591?hr_r=1
---
+++ script.js
@@ -0,0 +1,83 @@
+/*script.js*/ 
+let startTime;
+let interval;
+let running = false;
+let lapCounter = 0;
+let pausedTime = 0; 
+let accumulatedTime = 0;
+
+const display = document.getElementById('display');
+const startButton = document.getElementById('start');
+const pauseButton = document.getElementById('pause');
+const resetButton = document.getElementById('reset');
+const lapsList = document.getElementById('laps');
+const lapButton = document.getElementById('lap');
+
+startButton.addEventListener('click', start);
+pauseButton.addEventListener('click', pause);
+resetButton.addEventListener('click', reset);
+lapButton.addEventListener('click', lap);
+
+function start() {
+  if (!running) {
+    startTime = Date.now() - accumulatedTime;
+    interval = setInterval(updateDisplay, 10);
+    running = true;
+    startButton.disabled = true;
+    pauseButton.disabled = false;
+  }
+}
+
+function pause() {
+  if (running) {
+    clearInterval(interval);
+    accumulatedTime += Date.now() - startTime;
+    interval = null;
+    running = false;
+    startButton.disabled = false;
+    pauseButton.disabled = true;
+  }
+}
+
+function reset() {
+  clearInterval(interval);
+  interval = null;
+  running = false;
+  display.textContent = '00:00:00';
+  startButton.disabled = false;
+  pauseButton.disabled = true;
+  lapsList.innerHTML = '';
+}
+
+function updateDisplay() {
+  const elapsed = Date.now() - startTime;
+  const formattedTime = formatTime(elapsed);
+  display.textContent = formattedTime;
+}
+
+function formatTime(milliseconds) {
+  const totalSeconds = Math.floor(milliseconds / 1000);
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+  const millis = milliseconds % 1000;
+
+  return `${pad(minutes)}:${pad(seconds)}:${pad(millis, 3)}`;
+}
+
+function pad(num, size = 2) {
+  let s = num.toString();
+  while (s.length < size) {
+    s = '0' + s;
+  }
+  return s;
+}
+
+function lap() {
+  if (running) {
+    const lapTime = display.textContent;
+    lapCounter++;
+    const lapItem = document.createElement('li');
+    lapItem.textContent = `Lap ${lapCounter}: ${lapTime}`;
+    lapsList.appendChild(lapItem);
+  }
+}
